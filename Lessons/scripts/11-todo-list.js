590fbc418f268e9b4ee27603ec80b34ec4de27e5
Sebinatas <sebastianatas@gmail.com>
2023-08-14 15:08:57 +0300

    some homework but still needs a lot of work

--- Lessons/scripts/11-todo-list.js
+++ Lessons/scripts/11-todo-list.js
@@ -4,13 +4,29 @@
   4. Add it to the array
   5. console.log() the array */
 
-const todoList = [];                                                 // Step 1
+const todoList = ["test", "math", "15 Oct"]; // Step 1
+
+function renderTodoList() {
+    let todoListHTML = "";
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`;
+        todoListHTML += html;
+    }
+    //GENERATING THE HTML
+    console.log(todoListHTML);
+
+    document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
 
 function addTodo() {
-    const inputElement = document.querySelector(".js-name-input");   // Step 3
-    const name = inputElement.value;                                 // Step 3
-    todoList.push(name);                                             // Step 4
-    console.log(todoList);                                           // Step 5
+    const inputElement = document.querySelector(".js-name-input"); // Step 3
+    const name = inputElement.value; // Step 3
+    todoList.push(name); // Step 4
+    console.log(todoList); // Step 5
 
-    inputElement.value = '';         // text in text box becomes Empty after clicking Add
+    inputElement.value = ""; // text in text box becomes Empty after clicking Add
+
+    renderTodoList();
 }
